02d7e0cff0b2988235b68da891b679573a5daf3e
Grigory Sharov <sharov.grigory@gmail.com>
2021-06-22 16:17:44 +0100

    fix relion_ options formatting

--- mdcatch/schedule.py
+++ mdcatch/schedule.py
@@ -224,7 +224,7 @@ sigma_contrast          3
         file.write("{\n")
         for k, v in mapDict.items():
             if v != '':
-                file.write("'%s' : %s, \n" % (k, v))
+                file.write("'%s' : '%s',:q\n" % (k, v.replace('"', '')))
         file.write("}\n")
 
     # Save acquisition params
